1b154e2d4163c29472847e3834699f2906fed4bc
manucastelnovo <marnutskate@gmail.com>
2023-10-05 19:11:42 -0400

    video 47

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
-function ExpenseItem(){
-    return <h2>Expense Item!</h2>
-};
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>March 28th 2023</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$26.255</div>
+      </div>
+    </div>
+  );
+}
 
 export default ExpenseItem;

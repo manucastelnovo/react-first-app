4a219de350ecf188b7881edd43f958997b404970
manucastelnovo <marnutskate@gmail.com>
2023-10-06 10:34:25 -0400

    video 49

--- src/App.js
+++ src/App.js
@@ -24,21 +24,9 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      ></ExpenseItem>
+      <ExpenseItem expense={expense[0]}></ExpenseItem>
+      <ExpenseItem expense={expense[1]}></ExpenseItem>
+      <ExpenseItem expense={expense[2]}></ExpenseItem>
     </div>
   );
 }
